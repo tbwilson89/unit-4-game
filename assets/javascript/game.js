b699732728c0542f0fbd71cde6e0757287d6f89b
tbwilson89 <trevorbwilson89@gmail.com>
2019-02-24 14:52:18 -0600

    create and setup restart button, cleaned up winning and losing code in combat. Cleaned up what controls are active when having won or lost.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -56,6 +56,10 @@ gameObj = {
       $(`#${this.enemyChoice}`).remove()
       this.enemyChoice = ''
       this.wins++
+      if(this.wins === 3){
+        $('#game-info-header').text('You win!')
+        $('.btn-restart').addClass('btn-show')
+      }
       return;
     }
     this.characters[this.playerChoice].healthCurrent -= this.characters[this.enemyChoice].counter
@@ -63,45 +67,70 @@ gameObj = {
     $('.battle-log').append(`<p class="log-data ${this.enemyChoice}-log enemy-log text-light">${this.characters[this.enemyChoice].name} attacked ${this.characters[this.playerChoice].name} for ${this.characters[this.enemyChoice].counter}.</p>`)
     if(this.characters[this.playerChoice].healthCurrent <= 0){
       console.log('YOU LOSE')
+      $('#game-info-header').text('You lose...')
+      $('.btn-restart').addClass('btn-show')
       return;
+    }
+  },
+  resetFunction: function(){
+    $(`#${this.playerChoice}`).remove()
+    if(this.enemyChoice !== ''){
+      $(`#${this.enemyChoice}`).remove()
+    }
+    this.playerChoice = ''
+    this.enemyChoice = ''
+    $('.battle-log').removeClass('show')
+    $('.battle-actions').removeClass('show')
+    $('.btn-restart').removeClass('btn-show')
+    $('.battle-log').empty()
+    // Need to remove children of the battle-log to clear out previous game happenings.
+    this.slot = 1
+    for(char in this.characters){
+      this.characters[char].healthCurrent = this.characters[char].healthMax
+      this.characters[char].atkStack = 0
+      var html = $(`
+        <div id=${char} class='character-cards'>
+          <div class="card ${this.characters[char].cardColor}" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title text-center">${this.characters[char].name}</h5>
+            </div>
+            <img class="" src=${this.characters[char].img} alt="Card image cap">
+            <div class="card-body">
+              <p class="card-text text-center">Health: <span id="${char}-health">${this.characters[char].healthCurrent}</span></p>
+              <br>
+              <div class='text-center'>
+              </div>
+            </div>
+          </div>
+        </div>
+        `).appendTo(`#cs${this.slot}`)
+        this.slot++
+        $('.character-cards').on('click', (e)=>{
+          console.log('Card clicked')
+          console.log(e)
+          if(gameObj.playerChoice === ''){
+            gameObj.playerChoice = e.currentTarget.id
+            $(`#${e.currentTarget.id}`).appendTo('#cs-player')
+          } else if (gameObj.enemyChoice === '' && gameObj.playerChoice !== e.currentTarget.id){
+            gameObj.enemyChoice = e.currentTarget.id
+            $(`#${e.currentTarget.id}`).appendTo('#cs-enemy')
+            $('.battle-log').addClass('show')
+            $('.battle-actions').addClass('show')
+          }
+        })
     }
   }
 }
 
-for(char in gameObj.characters){
-  var html = 0
-  $(`
-    <div id=${char} class='character-cards'>
-      <div class="card ${gameObj.characters[char].cardColor}" style="width: 18rem;">
-        <div class="card-body">
-          <h5 class="card-title text-center">${gameObj.characters[char].name}</h5>
-        </div>
-        <img class="" src=${gameObj.characters[char].img} alt="Card image cap">
-        <div class="card-body">
-          <p class="card-text text-center">Health: <span id="${char}-health">${gameObj.characters[char].healthCurrent}</span></p>
-          <br>
-          <div class='text-center'>
-          </div>
-        </div>
-      </div>
-    </div>
-    `).appendTo(`#cs${gameObj.slot}`)
-    gameObj.slot++
-}
+gameObj.resetFunction()
 
-$('.character-cards').on('click', (e)=>{
-  console.log(e)
-  if(gameObj.playerChoice === ''){
-    gameObj.playerChoice = e.currentTarget.id
-    $(`#${e.currentTarget.id}`).appendTo('#cs-player')
-  } else if (gameObj.enemyChoice === '' && gameObj.playerChoice !== e.currentTarget.id){
-    gameObj.enemyChoice = e.currentTarget.id
-    $(`#${e.currentTarget.id}`).appendTo('#cs-enemy')
-    $('.battle-log').addClass('show')
-    $('.battle-actions').addClass('show')
+
+$('#atk-btn').on('click', ()=>{
+  if(gameObj.enemyChoice !== '' && gameObj.characters[gameObj.playerChoice].healthCurrent > 0){
+    gameObj.combatFunction()
   }
 })
-$('#atk-btn').on('click', ()=>{
-  gameObj.combatFunction()
-  // console.log(`${gameObj.characters[gameObj.playerChoice].atkStack} and ${gameObj.characters[gameObj.enemyChoice].healthCurrent}`)
+$('.btn-restart').on('click', ()=>{
+  console.log('Restarting game...')
+  gameObj.resetFunction()
 })
